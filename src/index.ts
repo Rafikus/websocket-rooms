9e3fda144e1d95c7a341b1885b82166c68761bba
rkusy <rkusy@microstrategy.com>
2022-12-13 09:56:49 +0100

    change to ts-node and add type

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,45 @@
 import express from "express";
 import * as http from "http";
-import * as WebSocket from "ws";
+import { Server, WebSocket } from "ws";
 
 const app = express();
 const server = http.createServer(app);
-const webSocketServer = new WebSocket.Server({ server });
+const webSocketServer = new Server({ server });
+let host: WebSocket | null = null;
+let currentStore = "";
 
-webSocketServer.on("connection", (webSocket: WebSocket) => {
-  webSocket.on("message", (message: string) => {
-    console.log("Message from client :: " + message);
-    webSocket.send("Echo :: " + message);
+webSocketServer.on("connection", (webSocket) => {
+  if (!host) {
+    host = webSocket;
+    webSocket.on("message", (data) => {
+      try {
+        const msg = JSON.parse(data.toString());
+        if (msg.type === "store_update") {
+          currentStore = JSON.stringify(msg.data);
+        }
+      } catch {
+        console.log("err");
+      }
+    });
+  }
+
+  webSocket.on("message", (data) => {
+    try {
+      console.log("Message from client :: " + data);
+      const type = JSON.parse(data.toString()).type;
+      const str = data.toString();
+      if (type !== "store_update") {
+        webSocketServer.clients.forEach((client) => {
+          if (client !== webSocket) {
+            client.send(str);
+          }
+        });
+      }
+    } catch {
+      console.log("err");
+    }
   });
-  webSocket.send("Welcome to chat !!");
 });
-
 
 server.listen(process.env.PORT || 8080, () => {
   console.log("Server started");
